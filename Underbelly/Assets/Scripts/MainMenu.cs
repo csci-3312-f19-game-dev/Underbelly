7cb9a6bec10c13e14dd0ec3ea4e36674ae1bddcb
Daniel Matthewson <danieltmatthewson@gmail.com>
2019-10-31 16:26:46 -0500

    Main Menu and Control Screen V1

---
+++ Underbelly/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //public GameManager gameManager;
+
+    //Takes the Player to SideA
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Spawn");
+    }
+
+    //Closes Application
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    //Takes the Player to the Controls and About Page
+    public void LoadControls ()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    //Takes the Player Back to the Main Menu
+    public void Back ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
