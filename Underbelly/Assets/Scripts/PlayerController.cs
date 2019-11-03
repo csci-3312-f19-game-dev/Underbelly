bbc113896673c815f09400dafab43e15273c4438
ssingh3-trinity <35928261+ssingh3-trinity@users.noreply.github.com>
2019-11-03 14:58:03 -0600

    player moving on spawn

--- Underbelly/Assets/Scripts/PlayerController.cs
+++ Underbelly/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerVelocity = 5.0f;
+    public float playerVelocity = 50.0f;
     Vector3 position;
 
     // Start is called before the first frame update
@@ -19,11 +19,11 @@ public class PlayerController : MonoBehaviour
         //we are getting input from left/right arrow keys using the input axis system:
         position.x += Input.GetAxis("Horizontal") * playerVelocity * Time.deltaTime;
         //"clamp" limits the value of a variable to a min or a max value:
-        position.x = Mathf.Clamp(position.x, -100f, 100f);
+        //position.x = Mathf.Clamp(position.x, -100f, 100f);
 
         position.y += Input.GetAxis("Vertical") * playerVelocity * Time.deltaTime;
         //max speed of player will be 0.8 in the forward direction:
-        position.y = Mathf.Clamp(position.y, -100f, 100f);
+        //position.y = Mathf.Clamp(position.y, -100f, 100f);
 
         transform.position = position;
     }
