32c63bfa130d1ec4dc8949929c26484946d07ee5
Daniel Matthewson <danieltmatthewson@gmail.com>
2019-11-09 13:51:14 -0600

    Fixed some of the main screens

---
+++ Underbelly/Assets/Scripts/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseMenu, PauseButton;
+
+    public void PauseGame() {
+        PauseMenu.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame() {
+        PauseMenu.SetActive(false);
+        PauseButton.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
