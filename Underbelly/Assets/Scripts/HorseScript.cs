40c79eed3ddb37895c0f56a27d0fc81ce2f30be2
Daniel Matthewson <danieltmatthewson@gmail.com>
2019-11-10 13:33:15 -0600

    Text and Dialogue

---
+++ Underbelly/Assets/Scripts/HorseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HorseScript : MonoBehaviour
+{
+    //public static bool hasCarrot;
+    //public static bool hasKnife;
+    public static int goodBoyPoints;
+    public bool hidden;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hidden = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hasCarrot = PlayerController.hasCarrot;
+        //hasKnife = PlayerController.hasKnife;
+        if (hidden || PlayerController.horseGone) transform.position = new Vector3(0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && PlayerController.inventory == "Carrot")
+        {
+            hidden = true;
+            PlayerController.goodBoyPoints += 1;
+            PlayerController.horseGone = true;
+        }
+        if (collision.gameObject.CompareTag("Player") && PlayerController.inventory == "Knife")
+        {
+            //SceneManager.LoadScene("MainMenu");
+            hidden = true;
+            PlayerController.goodBoyPoints -= 1;
+            PlayerController.horseGone = true;
+
+        }
+    }
+}
---
+++ Underbelly/Assets/Scripts/SceneTrackControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrackControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(GameObject.FindGameObjectWithTag("Player"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("EvilHorsePuzzle");
+            //SceneManager.MoveGameObjectToScene(gameObject.GetComponent(player), "EvilHorsePuzzle");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(755f, 568f);
+            SceneManager.LoadScene("Spawn");
+        }
+    }
+
+    
+}
